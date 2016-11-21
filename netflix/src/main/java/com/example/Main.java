08f09f7a1e0559d557eb61c9e4b22ab5dba1c347
Marco Alvarez <malvarez@Marcos-MBP.lan>
2016-11-21 00:33:16 -0600

    Se agrega la Tarea 3

--- netflix/src/main/java/com/example/Main.java
+++ netflix/src/main/java/com/example/Main.java
@@ -1,11 +1,84 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
 
+    //Definimos los ArrayList de películas y series y agregamos elementos en cada una de ellas
+
+        ArrayList <Pelicula> peliculas = new ArrayList<Pelicula>();
+        peliculas.add(new Pelicula("Cars", "Pixar"));
+        peliculas.add(new Pelicula("Toy Story", "Pixar"));
+        peliculas.add(new Pelicula("Aladdin", "Infantil", "Disney", 19014, 1990));
+        peliculas.add(new Pelicula("Shrek", "Comedia", "Dreamworks", 26789, 2001));
+        peliculas.add(new Pelicula("Trolls", "Dreamworks"));
+
+        ArrayList <Serie> series = new ArrayList<Serie>();
+        series.add(new Serie("Orange is the New Black", "NTFLX"));
+        series.add(new Serie("The Blacklist", "Drama", "NBC", 3605, 4));
+        series.add(new Serie("Scorpion", "Action", "CBS", 3890, 8));
+        series.add(new Serie("The Fall", "CBS"));
+        series.add(new Serie("Narcos", "Drama", "NTFLX", 4750, 2));
+
+    //Aquí podría haber errores por lo que se agregó un try
+        try {
+
+            //en esta sección se marcan como visto las películas y/o series, y la función captura la duración total de éstas
+            peliculas.get(2).marcarVisto();
+            peliculas.get(3).marcarVisto();
+
+            series.get(1).marcarVisto();
+            series.get(2).marcarVisto();
+            series.get(4).marcarVisto();
+            series.get(15).marcarVisto(); //esta línea fue agregada como error para probar el funcionamiento de esta parte
+
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido el siguiente error: ");
+            e.printStackTrace();
+
+        } finally {
+            System.out.println(":)");
+        }
+
+      //Se realizaron operaciones para determinar la película y serie más vista
+        int mayorAño = peliculas.get(1).getAño();
+        int peliculaMasReciente = 1;
+
+        for (int i = 0; i < peliculas.size(); i++) {
+
+            if (peliculas.get(i).esVisto()) {
+                System.out.println("Pelicula vista: " + peliculas.get(i).getTitulo() + " - " + peliculas.get(i).tiempoVisto());
+            }
+
+            if (peliculas.get(i).getAño() > mayorAño){
+                peliculaMasReciente = i;
+            }
+        }
+
+        int masTemporadas = series.get(1).getTemporadas();
+        int serieConMasTemporadas = 1;
+
+        for (int i = 0; i < series.size(); i++) {
+
+            if (series.get(i).esVisto()) {
+                System.out.println("Serie vista: " + series.get(i).getTitulo() + " - " + series.get(i).tiempoVisto());
+            }
+
+            if (series.get(i).getTemporadas() > masTemporadas){
+                serieConMasTemporadas = i;
+            }
+        }
+
+        //El sistema imprime la película y serie más vistas
+        System.out.println("Pelicula más reciente: " + peliculas.get(peliculaMasReciente).toString());
+        System.out.println("Serie con más temporadas: " + series.get(serieConMasTemporadas).toString());
+
+
+/*
         Pelicula[] peliculasArray = new Pelicula[5];
         Serie[] seriesArray = new Serie[5];
 
@@ -58,6 +131,6 @@ public class Main {
 
         System.out.println("Pelicula más reciente: " + peliculasArray[peliculaMasReciente].toString());
         System.out.println("Serie con más temporadas: " + seriesArray[serieConMasTemporadas].toString());
-
+*/
     }
 }
